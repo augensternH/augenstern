f9eaca859da080c837e698e2ed890df8eff309b2
augensternH <hoanghoa38sos@gmail.com>
2022-03-05 20:00:30 +0700

    init

---
+++ src/component/pageComponent/bodyComponent/CartPage.js
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from "./cartpage.module.css";
+import Procar from "./Procar";
+import clsx from "clsx";
+const CartPage = () => {
+  return (
+    <>
+      <div className={styles}>
+        <div className={clsx(styles.wrapW)}>
+          <div className={styles.wrap}>
+            <div className={clsx(styles.row)}>
+              <div className={styles.col1}>Sản phẩm</div>
+              <div className={styles.col2}>Đơn giá</div>
+              <div className={styles.col2}>Số lượng</div>
+              <div className={styles.col2}>Tổng tiền</div>
+              <div className={styles.col2}>Hoạt động</div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.wrap}>
+          <Procar />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CartPage;
