162e189560d4763d4f0dbb878bee0b25639237a2
augensternH <hoanghoa38sos@gmail.com>
2022-03-05 20:58:25 +0700

    Updates

---
+++ precache-manifest.c2e4e26cd6459f889e6a0aa081deb69c.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "bbf81c1dd657ff7fea04",
+    "url": "/augensternH/augenstern/static/css/main.3f141ae0.chunk.css"
+  },
+  {
+    "revision": "bbf81c1dd657ff7fea04",
+    "url": "/augensternH/augenstern/static/js/main.bbf81c1d.chunk.js"
+  },
+  {
+    "revision": "96bae0090b21b634040c",
+    "url": "/augensternH/augenstern/static/css/1.93a5130e.chunk.css"
+  },
+  {
+    "revision": "96bae0090b21b634040c",
+    "url": "/augensternH/augenstern/static/js/1.96bae009.chunk.js"
+  },
+  {
+    "revision": "1f0e9fc95fa65dd717f7",
+    "url": "/augensternH/augenstern/static/js/2.1f0e9fc9.chunk.js"
+  },
+  {
+    "revision": "4d9d090e50477b225481",
+    "url": "/augensternH/augenstern/static/css/3.c4ab15b6.chunk.css"
+  },
+  {
+    "revision": "4d9d090e50477b225481",
+    "url": "/augensternH/augenstern/static/js/3.4d9d090e.chunk.js"
+  },
+  {
+    "revision": "22338ce76ab2f6fb9d79",
+    "url": "/augensternH/augenstern/static/js/4.22338ce7.chunk.js"
+  },
+  {
+    "revision": "8a9de7cf4ddbaa11b8b3",
+    "url": "/augensternH/augenstern/static/css/5.cb356c88.chunk.css"
+  },
+  {
+    "revision": "8a9de7cf4ddbaa11b8b3",
+    "url": "/augensternH/augenstern/static/js/5.8a9de7cf.chunk.js"
+  },
+  {
+    "revision": "c3fffd1ae0de73def293",
+    "url": "/augensternH/augenstern/static/js/6.c3fffd1a.chunk.js"
+  },
+  {
+    "revision": "2707fd826b55a4cf581c",
+    "url": "/augensternH/augenstern/static/js/7.2707fd82.chunk.js"
+  },
+  {
+    "revision": "c62d513122d3c7c11bd1",
+    "url": "/augensternH/augenstern/static/js/runtime~main.c62d5131.js"
+  },
+  {
+    "revision": "53d6ecb2c04ed9f53b94045a4d62bc21",
+    "url": "/augensternH/augenstern/static/media/cart.53d6ecb2.png"
+  },
+  {
+    "revision": "16f12759e60071709496dcb9d3f3cb02",
+    "url": "/augensternH/augenstern/static/media/img-1.16f12759.png"
+  },
+  {
+    "revision": "71afc93135f72777ea667a2b9dfb650f",
+    "url": "/augensternH/augenstern/static/media/img-2.71afc931.png"
+  },
+  {
+    "revision": "5870f6701023a4c687b8bd1c7d56dd91",
+    "url": "/augensternH/augenstern/static/media/img-3.5870f670.png"
+  },
+  {
+    "revision": "da3d937474731b02241601bec420705d",
+    "url": "/augensternH/augenstern/static/media/product-1.da3d9374.png"
+  },
+  {
+    "revision": "4fe401d528dee33a6bf2b0854d29aac0",
+    "url": "/augensternH/augenstern/static/media/user.4fe401d5.png"
+  },
+  {
+    "revision": "fe1eb62fc358713303a8fa86c87ce288",
+    "url": "/augensternH/augenstern/static/media/love.fe1eb62f.png"
+  },
+  {
+    "revision": "6822dd515420ba640ee659f722020144",
+    "url": "/augensternH/augenstern/static/media/login.6822dd51.png"
+  },
+  {
+    "revision": "91e5a0bbeb3c9112bfd5ecf1c6c0d172",
+    "url": "/augensternH/augenstern/static/media/signin.91e5a0bb.png"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/augensternH/augenstern/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/augensternH/augenstern/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/augensternH/augenstern/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/augensternH/augenstern/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "4cd8420cc83ba69a861b579fef8726ef",
+    "url": "/augensternH/augenstern/index.html"
+  }
+];
